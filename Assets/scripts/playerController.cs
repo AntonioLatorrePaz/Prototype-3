d85e6559944773369d6626b4806f09e64bdd3ffd
João Antônio Latorre Paz <jalatorrepaz@gmail.com>
2021-04-24 16:03:10 -0300

    Prototype 3

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float speed = 10f;
+    public float xRange = 10f;
+    public GameObject projectilePrefabs;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefabs, transform.position, projectilePrefabs.transform.rotation);
+        }
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+    }
+}
